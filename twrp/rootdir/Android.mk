f429288048e90aa3470c928a773c9c5631a4b1ef
lawn <bloom.all.over.12@gmail.com>
2016-04-10 09:49:19 +0900

    typo
    Change-Id: I84f28e96ff6550343f3d081851f7bdc33cae7a3b

--- twrp/rootdir/Android.mk
+++ twrp/rootdir/Android.mk
@@ -4,6 +4,6 @@ include $(CLEAR_VARS)
 LOCAL_MODULE       := twrp.fstab
 LOCAL_MODULE_TAGS  := optional eng
 LOCAL_MODULE_CLASS := RECOVERY_EXECUTABLES
-LOCAL_SRC_FILES    := etc/fstab.qcom
+LOCAL_SRC_FILES    := etc/twrp.fstab
 LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
